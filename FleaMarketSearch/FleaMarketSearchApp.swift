d5122259ac0740f6bd3bf1ee5931b73f7d58276b
kabikira <kabikirar2@protonmail.com>
2022-11-13 22:41:18 +0900

    Initial Commit

---
+++ FleaMarketSearch/FleaMarketSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FleaMarketSearchApp.swift
+//  FleaMarketSearch
+//
+//  Created by koala panda on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct FleaMarketSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
