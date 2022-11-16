beb772b853ae6cfe1db64920bf4a751c80d9b87e
kabikira <kabikirar2@protonmail.com>
2022-11-16 22:55:22 +0900

    add WKWebViewの読み込み状況をUIProgressViewで表示

--- FleaMarketSearch/WebView.swift
+++ FleaMarketSearch/WebView.swift
@@ -9,19 +9,63 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
+    var webView = WKWebView()
+    var progressView = UIProgressView()
+    
     let url: String
     
-    func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
+    class Coordinator: NSObject {
+        var parent: WebView
+        
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+        
+        // WebViewの読み込み状況を監視する
+        func addProgressObserver() {
+            parent.webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        }
+        
+        override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+            // progressViewのアニメーション処理
+            if keyPath == "estimatedProgress" {
+                parent.progressView.alpha = 1.0
+                parent.progressView.setProgress(Float(parent.webView.estimatedProgress), animated: true)
+                
+                if parent.webView.estimatedProgress >= 1.0 {
+                    UIView.animate(withDuration: 0.2, delay: 0.0, options: [.curveEaseOut], animations: { [weak self] in
+                        self?.parent.progressView.alpha = 0.0
+                    }, completion: { (finished: Bool) in
+                        self.parent.progressView.setProgress(0.0, animated: false)
+                    })
+                }
+            }
+        }
     }
     
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        uiView.allowsBackForwardNavigationGestures = true
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        webView.addSubview(progressView)
+        
+        // UIProgressViewのレイアウト設定
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.widthAnchor.constraint(equalTo: webView.widthAnchor, multiplier: 1.0).isActive = true
+        progressView.topAnchor.constraint(equalTo: webView.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        progressView.leadingAnchor.constraint(equalTo: webView.leadingAnchor, constant: 0).isActive = true
+        
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        context.coordinator.addProgressObserver()
+        webView.allowsBackForwardNavigationGestures = true
         guard let url = URL(string: url) else {
             return
         }
         let request = URLRequest(url: url)
-        uiView.load(request)
-
+        webView.load(request)
     }
 }
