96d19187b27c0aa9e34fcaf9ad1cfadd85cd8345
kabikira <kabikirar2@protonmail.com>
2022-11-22 18:58:53 +0900

    add SubView

--- FleaMarketSearch/ContentView.swift
+++ FleaMarketSearch/ContentView.swift
@@ -22,40 +22,10 @@ struct ContentView: View {
             } else {
                 SearchScreenView(word: $word)
                 Spacer()
-                    .frame(height: 30)
-                    Button {
-                        isShowingView.toggle()
-                        // 10個より多く要素が入っていたらインデックス0から削除
-                        if words.count > 10 {
-                            words.removeFirst()
-                        }
-                        // 一旦配列に保存し
-                        words.append(word)
-                        print("ContentView",words)
-                        // ここにユーザデフォルトに保存する処理
-                        userDefaultsOp.wordsSet(words: words)
-                    } label: {
-                        Text("検索")
-                            .font(.title)
-                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/6)
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                
-                    Button {
-                        showingSheet.toggle()
-                    } label: {
-                        Text("履歴")
-                            .font(.title)
-                            .frame(width: UIScreen.main.bounds.width/4, height: UIScreen.main.bounds.width/6)
-                    }
-                
-                    .buttonStyle(.bordered)
-                    .sheet(isPresented: $showingSheet) {
-                        WordsView(showingSheet: $showingSheet, word: $word, words: $words)
-                    }
-                Spacer()
-                    .frame(height: 100)
+                    .frame(height: 20)
+                HStack {
+                    SearchButtonView(word: $word, words: $words, showingSheet: $showingSheet, isShowingView: $isShowingView, userDefaultsOp: userDefaultsOp)
+                }
             }
         }
     }
