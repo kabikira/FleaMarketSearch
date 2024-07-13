ac03e1f560373be30f7d1752609301f470b2e65e
kabikira <kabikirar2@protonmail.com>
2024-07-13 15:53:29 +0900

    fix プログレスバーが動作するよう修正

--- FleaMarketSearch/WebView.swift
+++ FleaMarketSearch/WebView.swift
@@ -1,7 +1,17 @@
+//
+//  WebView.swift
+//  FleaMarketSearch
+//
+//  Created by koala panda on 2022/11/16.
+//
+
 import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
+    var webView = WKWebView()
+    var progressView = UIProgressView()
+
     let url: String
 
     class Coordinator: NSObject, WKNavigationDelegate {
@@ -9,6 +19,27 @@ struct WebView: UIViewRepresentable {
 
         init(_ parent: WebView) {
             self.parent = parent
+        }
+
+        // WebViewの読み込み状況を監視する
+        func addProgressObserver() {
+            parent.webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        }
+
+        override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+            // progressViewのアニメーション処理
+            if keyPath == "estimatedProgress" {
+                parent.progressView.alpha = 1.0
+                parent.progressView.setProgress(Float(parent.webView.estimatedProgress), animated: true)
+
+                if parent.webView.estimatedProgress >= 1.0 {
+                    UIView.animate(withDuration: 0.2, delay: 0.0, options: [.curveEaseOut], animations: { [weak self] in
+                        self?.parent.progressView.alpha = 0.0
+                    }, completion: { (finished: Bool) in
+                        self.parent.progressView.setProgress(0.0, animated: false)
+                    })
+                }
+            }
         }
 
         // ページ読み込み完了時に呼ばれる
@@ -40,7 +71,6 @@ struct WebView: UIViewRepresentable {
                 // 初回実行
                 hideElement('.merNavigationBottom');
                 hideElement('.page-header');
-
                 """
             webView.evaluateJavaScript(hiddenProfileScript) { _, error in
                 if let error = error {
@@ -55,30 +85,25 @@ struct WebView: UIViewRepresentable {
     }
 
     func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
         webView.navigationDelegate = context.coordinator
-
-        let progressView = UIProgressView(progressViewStyle: .default)
         webView.addSubview(progressView)
 
         // UIProgressViewのレイアウト設定
         progressView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            progressView.widthAnchor.constraint(equalTo: webView.widthAnchor),
-            progressView.topAnchor.constraint(equalTo: webView.safeAreaLayoutGuide.topAnchor),
-            progressView.leadingAnchor.constraint(equalTo: webView.leadingAnchor)
-        ])
-
-        // URLリクエストの作成とロード
-        if let url = URL(string: url) {
-            let request = URLRequest(url: url)
-            webView.load(request)
-        }
+        progressView.widthAnchor.constraint(equalTo: webView.widthAnchor, multiplier: 1.0).isActive = true
+        progressView.topAnchor.constraint(equalTo: webView.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        progressView.leadingAnchor.constraint(equalTo: webView.leadingAnchor, constant: 0).isActive = true
 
         return webView
     }
 
     func updateUIView(_ webView: WKWebView, context: Context) {
-        // updateUIViewではなく、makeUIView内で設定するのでここは空でOK
+        context.coordinator.addProgressObserver()
+        webView.allowsBackForwardNavigationGestures = true
+        guard let url = URL(string: url) else {
+            return
+        }
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
 }
